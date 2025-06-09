59c052ce1bbfb314c5b5c68b8bf9c07a8b7c2835
souzajr <robertos.cps@gmail.com>
2025-06-09 15:12:03 -0300

    check PR

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,9 @@
 import js from '@eslint/js';
 import { FlatCompat } from '@eslint/eslintrc';
 import eslintPluginTypeScript from '@typescript-eslint/eslint-plugin';
+import typeScriptParser from '@typescript-eslint/parser';
 import prettierConfig from 'eslint-config-prettier';
+import globals from 'globals';
 
 const compat = new FlatCompat({
   baseDirectory: import.meta.dirname,
@@ -10,17 +12,26 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.config({
-    extends: [
-      'eslint:recommended',
-      'next',
-      'prettier',
-      'plugin:prettier/recommended',
-    ],
+    extends: ['eslint:recommended', 'prettier', 'plugin:prettier/recommended'],
     plugins: ['@typescript-eslint', 'prettier'],
-    ignorePatterns: ['node_modules/', '.next/', '*.mjs'],
+    ignorePatterns: ['node_modules/', '*.mjs'],
   }),
   {
     files: ['src/**/*.{ts,tsx,js}'],
+    languageOptions: {
+      parser: typeScriptParser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+      globals: {
+        ...globals.browser,
+        ...globals.es2021,
+      },
+    },
     rules: {
       ...eslintPluginTypeScript.configs.recommended.rules,
       ...prettierConfig.rules,
