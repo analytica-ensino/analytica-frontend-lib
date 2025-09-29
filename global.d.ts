54f4b1ebdbef0136dcc2474cdcfb5bc594c604eb
William JS <williamj.silva@live.com>
2025-09-29 09:48:27 -0300

    removing uncessary change

--- global.d.ts
+++ global.d.ts
@@ -16,9 +16,4 @@ declare module '*.jpeg' {
 declare module '*.svg' {
   const content: string;
   export default content;
-}
-
-declare module '*.css' {
-  const content: string;
-  export default content;
-}
+} 
