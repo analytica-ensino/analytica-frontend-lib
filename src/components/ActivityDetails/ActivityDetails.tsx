e7118cabbe30ca5ced2553c63005dcbc5af134be
William JS <williamj.silva@live.com>
2025-12-29 17:17:30 -0300

    fix files

--- src/components/ActivityDetails/ActivityDetails.tsx
+++ src/components/ActivityDetails/ActivityDetails.tsx
@@ -678,6 +678,7 @@ export const ActivityDetails = ({
 
       {/* Correct Activity Modal */}
       <CorrectActivityModal
+        key={correctionData?.studentId || 'modal'}
         isOpen={isModalOpen}
         onClose={handleCloseModal}
         data={correctionData}
